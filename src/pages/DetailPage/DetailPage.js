febaa8d25101e424f318429439b66827a81f8699
kimboyoon <kimboyoon0908@gmail.com>
2022-11-15 21:17:34 +0900

    ADD: Detail Page 구현중

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -3,10 +3,18 @@ import './DetailPage.scss';
 
 const DetailPage = () => {
   const [comment, setComment] = useState('');
+  const [commentList, setCommentList] = useState([]);
 
   const handleComment = e => {
     setComment(e.target.value);
   };
+
+  const saveComment = () => {
+    setCommentList([...commentList, comment]);
+    setComment('');
+  };
+
+  const valid = comment.length >= 10 ? false : true;
 
   return (
     <div className="detailPage">
@@ -42,40 +50,48 @@ const DetailPage = () => {
             <button className="payBtn">바로 수강하기</button>
             <button className="cartBtn">장바구니 담기</button>
           </div>
+          <ul className="payFormInfo">
+            <li>총 10개 수업 (11시간 1분)</li>
+            <li>수강기한: 무제한</li>
+            <li>난이도: 초급</li>
+          </ul>
         </form>
         <div className="content">
           <div className="introduceTitle" id="introduce">
             강의소개
           </div>
+          <div>가지고 있는 장비만으로 실패 없이 집빵을 만들고 싶다면?</div>
           <div>
-            <div id="curriculum">커리큘럼입니다.</div>
+            <div className="curriTitle" id="curriculum">
+              커리큘럼
+            </div>
             <div>
-              <div>
-                <i class="fa-regular fa-circle-play" />
+              <div className="curriList">
+                <i className="fa-regular fa-circle-play" />
                 <span> 0. Intro</span>
               </div>
-              <div>
-                <i class="fa-regular fa-circle-play" />
-                <span> 0. Intro</span>
+              <div className="curriList">
+                <i className="fa-regular fa-circle-play" />
+                <span> 1. 생크림모닝빵</span>
               </div>
-              <div>
-                <i class="fa-regular fa-circle-play" />
-                <span> 0. Intro</span>
+              <div className="curriList">
+                <i className="fa-regular fa-circle-play" />
+                <span> 2. 기본 식빵</span>
               </div>
-              <div>
-                <i class="fa-regular fa-circle-play" />
-                <span> 0. Intro</span>
+              <div className="curriList">
+                <i className="fa-regular fa-circle-play" />
+                <span> 3. 마늘빵</span>
               </div>
-              <div>
-                <i class="fa-regular fa-circle-play" />
-                <span> 0. Intro</span>
+              <div className="curriList">
+                <i className="fa-regular fa-circle-play" />
+                <span> 4. 단팥빵</span>
               </div>
-              <div>
-                <i class="fa-regular fa-circle-play" />
-                <span> 0. Intro</span>
+              <div className="curriList">
+                <i className="fa-regular fa-circle-play" />
+                <span> 5. 찹쌀도넛</span>
               </div>
-              <div>
-                <i class="fa-regular fa-circle-play" />
+              <div className="curriList">
+                <i className="fa-regular fa-circle-play" />
                 <span> 0. Intro</span>
               </div>
             </div>
@@ -87,15 +103,50 @@ const DetailPage = () => {
             <div className="reviewSubTitle">직접 작성하신 수강평입니다.</div>
             <div className="inputBox">
               <div className="inputBoxStar">별점을 선택해주세요</div>
-              <input
+              <textarea
                 type="text"
                 placeholder="수강평을 10자 이상 작성해주세요."
                 onChange={handleComment}
+                value={comment}
               />
-              <button className="enrollBtn">등록</button>
+              <button
+                className="enrollBtn"
+                onClick={saveComment}
+                disabled={valid}
+              >
+                등록
+              </button>
             </div>
+            <div className="commentSort">
+              <span>View</span>
+              <span>최신 순</span>
+              <span>좋아요 순</span>
+              <span>높은평점 순</span>
+              <span>낮은평점 순</span>
+            </div>
+
+            {commentList.map((comment, key) => {
+              return (
+                <div key={key} className="commentList">
+                  <div className="commentInfo">
+                    <img
+                      src="../../images/baking.jpg"
+                      className="profileImg"
+                      alt="profileImage"
+                    />
+                    <div className="commentStarId">
+                      <div className="commentStar">Star</div>
+                      <div className="commentId">id</div>
+                    </div>
+                  </div>
+                  <div className="comment">{comment}</div>
+                </div>
+              );
+            })}
           </div>
-          <div id="qna">Q & A</div>
+          <div>
+            <div id="qna">Q & A</div>
+          </div>
         </div>
       </div>
     </div>
