ee541512ff35eba211d9fc56345e23d871774f90
sumin0725 <suminxx12x@gmail.com>
2022-11-15 18:35:14 +0900

    ADD:메인 5%완료 작업중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,20 @@
 import './Main.scss';
 
 const Main = () => {
-  return <h1>Hi</h1>;
+  return (
+    <div className="mainBody">
+      <div className="mainColorBox">
+        <div className="colorBoxContent">
+          <p className="colorBoxText">
+            인터넷 요리강의 No.1 <span>P!CKEAT</span>에서
+          </p>
+        </div>
+        <div className="mainImgBox">
+          <img className="mainImg" src="images/채소사진.jpg" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
