c1aed94419d87044958942a0e58975f41693a9d4
kimboyoon <kimboyoon0908@gmail.com>
2022-11-17 01:36:43 +0900

    ADD: 장바구니 레이아웃 구현 완료

--- src/pages/ShoppingCart/ShoppingCart.js
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -4,15 +4,53 @@ const ShoppingCart = () => {
   return (
     <div className="container">
       <section className="sectionCart">
-        <div className="sectionCartHeader">장바구니</div>
+        <header className="sectionCartHeader">장바구니</header>
         <nav className="sectionCartControl">
-          <input type="checkbox" />
+          <input className="cartCheckbox" type="checkbox" />
           <label>전체선택</label>
-          <button>선택삭제</button>
+          <button className="deleteBtn">선택삭제 ✖</button>
         </nav>
 
-        <div>강의</div>
-        <div>상품</div>
+        <main className="sectionCartBody">
+          <div className="cartLectureList">
+            <input className="cartCheckbox" type="checkbox" />
+            <img
+              className="cartLectureImg"
+              src="../../images/baking.jpg"
+              alt="lectureImg"
+            />
+            <div className="cartLectureInfo">
+              <div className="cartLectureTitle">강의제목강의제목</div>
+              <span className="lectureInstructor">선생님</span>
+              <span className="lectureDuration">무제한 수강</span>
+            </div>
+            <div className="cartLecturePrice">
+              <span>55,000</span>원
+            </div>
+          </div>
+          <div className="cartProductList">
+            <input className="cartCheckbox" type="checkbox" />
+            <img
+              className="cartLectureImg"
+              src="../../images/baking.jpg"
+              alt="lectureImg"
+            />
+            <div className="cartProductInfo">
+              <div className="productName">도구도구도구</div>
+              <div className="productCount">
+                <span className="productCountHeader">수량</span>
+                <div className="productCountBtn">
+                  <button>–</button>
+                  <span>2</span>
+                  <button>+</button>
+                </div>
+              </div>
+            </div>
+            <div className="cartProductPrice">
+              <span>55,000</span>원
+            </div>
+          </div>
+        </main>
       </section>
 
       <aside className="asideCart">
