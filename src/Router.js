1f0a99ee28eb5fcc9d869347a99dfcbdd0733924
kimboyoon <kimboyoon0908@gmail.com>
2022-11-17 22:07:21 +0900

    ADD: test

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import DetailPage from './pages/DetailPage/DetailPage';
+import ProductDetailPage from './pages/DetailPage/ProductDetailPage';
 import Payment from './pages/Payment/Payment';
 import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
 import SignUp from './pages/SignUp/SignUp';
@@ -13,6 +14,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/detailpage" element={<DetailPage />} />
+        <Route path="/productdetailpage" element={<ProductDetailPage />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/shoppingcart" element={<ShoppingCart />} />
         <Route path="/signup" element={<SignUp />} />
---
+++ src/pages/DetailPage/ProductDetailPage.js
@@ -0,0 +1,16 @@
+import './ProductDetailPage.scss';
+
+const ProductDetailPage = () => {
+  return (
+    <div>
+      <main>
+        <img src="" alt="" />
+        <section>
+          <header>다용도 주방 가위</header>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default ProductDetailPage;
