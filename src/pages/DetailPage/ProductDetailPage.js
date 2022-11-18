73ae552bf58899059a555428e123fdeff219089a
kimboyoon <kimboyoon0908@gmail.com>
2022-11-18 22:12:20 +0900

    ADD: 레이아웃 작업중

--- src/pages/DetailPage/ProductDetailPage.js
+++ src/pages/DetailPage/ProductDetailPage.js
@@ -2,7 +2,7 @@ import './ProductDetailPage.scss';
 
 const ProductDetailPage = () => {
   return (
-    <div>
+    <div className="productDetailPage">
       <header className="product">
         <img
           className="productImg"
@@ -15,32 +15,47 @@ const ProductDetailPage = () => {
           <div className="productPrice">30000원</div>
           <div className="productDelivery">
             <div className="productDeliveryTitle">배송</div>
-            <div className="productDeliveryPrice">3000원</div>
-            <div className="productDeliveryCondition">
-              5만원 이상 주문시 무료배송
+            <div className="productDeliveryContent">
+              <div className="productDeliveryPrice">3000원</div>
+              <div className="productDeliveryCondition">
+                5만원 이상 주문시 무료배송
+              </div>
             </div>
           </div>
+
           <div className="productSeller">
             <div className="productSellerTitle">판매자</div>
-            <div className="productSellerName">PICKEAT</div>
-          </div>
-          <div className="productOption">
-            <div className="productOptionTitle">수량</div>
-            <div className="prodcutOptionAmount">1개</div>
+            <div className="productSellerName">P!CKEAT</div>
           </div>
 
-          <div className="productSelectedPrice">주문금액</div>
+          <div className="productOption">
+            <div className="productOptionTitle">수량</div>
+            <div className="productOptionAmount">
+              <button className="decreaseBtn">－</button>
+              <span className="amount">개수</span>
+              <button className="increaseBtn">＋</button>
+            </div>
+          </div>
+
+          <div className="productSelected">
+            <div className="productSelectedTitle">주문금액</div>
+            <div className="productSelectedPrice">60000원</div>
+          </div>
+
+          <div className="payFormBtns">
+            <button className="cartBtn">장바구니</button>
+            <button className="payBtn">바로구매</button>
+          </div>
         </container>
       </header>
 
-      <main>
-        <nav>
-          <ul>
-            <li>상품정보</li>
-            <li>후기</li>
-            <li>문의</li>
-          </ul>
+      <main className="productDetailPageMain">
+        <nav className="productDetailPageMainNavBar">
+          <div>상품정보</div>
+          <div>후기</div>
+          <div>문의</div>
         </nav>
+
         <aside>
           <div>수량</div>
           <div>1개</div>
