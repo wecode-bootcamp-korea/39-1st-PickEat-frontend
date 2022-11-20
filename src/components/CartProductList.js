71c845c3464c75e4f7fe658beb165c09ca7e1a0a
kimboyoon <kimboyoon0908@gmail.com>
2022-11-21 00:47:24 +0900

    ADD: 체크박스 구현중..

--- src/components/CartProductList.js
+++ src/components/CartProductList.js
@@ -3,17 +3,11 @@ import React from 'react';
 const CartProductList = props => {
   const { id, title, price, img, quantity } = props.product;
   const { increaseBtn, decreaseBtn } = props;
-  // const { checked } = props;
   const { deleteBtn } = props;
 
   return (
     <div className="cartProductList" key={id}>
-      <input
-        className="cartCheckbox"
-        type="checkbox"
-        // onChange={checked}
-        defaultChecked={true}
-      />
+      <input className="cartCheckbox" type="checkbox" defaultChecked={true} />
       <img className="cartLectureImg" src={img} alt="lectureImg" />
       <div className="cartProductInfo">
         <div className="productName">{title}</div>
