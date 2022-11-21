69c3cb8a39efbba1b8b8e7386195f4138389bfb9
kimboyoon <kimboyoon0908@gmail.com>
2022-11-21 15:25:41 +0900

    ADD: rate 추가중

--- src/pages/DetailPage/ProductDetailPage.js
+++ src/pages/DetailPage/ProductDetailPage.js
@@ -1,6 +1,19 @@
+import React, { useState } from 'react';
 import './ProductDetailPage.scss';
 
 const ProductDetailPage = () => {
+  const [review, setReview] = useState([]);
+  const [reviewList, setReviewList] = useState([]);
+
+  const handleReview = e => {
+    setReview(e.target.value);
+  };
+
+  const saveReview = () => {
+    setReviewList([...reviewList, review]);
+    setReview('');
+  };
+
   return (
     <div className="productDetailPage">
       <header className="product">
@@ -52,8 +65,8 @@ const ProductDetailPage = () => {
       <main className="productDetailPageMain">
         <nav className="productDetailPageMainNavBar">
           <div className="navBarInfo">상품정보</div>
-          <div className="navBarReview">후기</div>
-          <div className="navBarCall">문의</div>
+          <div className="navBarReview">리뷰</div>
+          <div className="navBarCall">자주하는 질문</div>
         </nav>
 
         <aside className="productDetailPageAside">
@@ -98,13 +111,63 @@ const ProductDetailPage = () => {
             </div>
             <div className="reviewSubTitle">직접 작성하신 리뷰입니다.</div>
             <div className="inputBox">
+              <div className="ratingStar">
+                <div className="ratingBackGroundImg">
+                  {/* <div className="rating" style={width: productData.rate}/> */}
+                </div>
+              </div>
               <div className="inputBoxStar">별점을 선택해주세요</div>
               <textarea
                 className="reviewInputBox"
                 type="text"
                 placeholder="리뷰를 10자 이상 작성해주세요."
+                onChange={handleReview}
+                value={review}
               />
-              <button className="enrollBtn">등록</button>
+              <button className="enrollBtn" onClick={saveReview}>
+                등록
+              </button>
+            </div>
+          </div>
+
+          {reviewList.map((comment, key) => {
+            return (
+              <div key={key} className="commentList">
+                <div className="commentInfo">
+                  <img
+                    src="../../images/product.jpg"
+                    className="profileImg"
+                    alt="profileImage"
+                  />
+                  <div className="commentStarId">
+                    <div className="commentStar">Star</div>
+                    <div className="commentId">id</div>
+                  </div>
+                </div>
+                <div className="comment">{comment}</div>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="productQna">
+          <div className="productQnaTitle">자주하는 질문</div>
+
+          <div>
+            <div className="productQnaQuestion">
+              교환(반품) 진행 시, 배송비가 부과되나요?
+            </div>
+            <div className="productQnaAnswer">
+              - 단순 변심에 의한 교환/반품 시 배송비 6,000원 (주문건 배송비를 낸
+              경우 3,000원)을 고객님께서 부담하셔야 합니다.
+            </div>
+
+            <div className="productQnaQuestion">
+              구매 영수증은 어떻게 발급 받나요?
+            </div>
+            <div className="productQnaAnswer">
+              - 구매하신 주문건의 영수증 출력과 관련하여 직접 조회가
+              어려우십니다. 해당 내용은 PiCKEAT 고객센터로 문의 부탁드립니다.
             </div>
           </div>
         </div>
