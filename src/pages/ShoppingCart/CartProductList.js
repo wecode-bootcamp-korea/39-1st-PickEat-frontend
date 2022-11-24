ec6b163b8e88eb1f400bbf42f56755404aee1656
kimboyoon <kimboyoon0908@gmail.com>
2022-11-24 14:40:31 +0900

    ADD: 새로운 브랜치 다시 생성함

---
+++ src/pages/ShoppingCart/CartProductList.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './CartProductList.scss';
+
+const CartProductList = props => {
+  const { id, productId, title, price, image, quantity } = props.product;
+  const { increaseBtn, decreaseBtn, deleteBtn, checkItems, setCheckItems } =
+    props;
+
+  const handleSingleCheck = (checked, id) => {
+    if (checked) {
+      setCheckItems(prev => [...prev, id]);
+    } else {
+      setCheckItems(checkItems.filter(el => el !== id));
+    }
+  };
+  return (
+    <div className="cartProductList" key={id}>
+      <input
+        className="cartCheckbox"
+        type="checkbox"
+        defaultChecked={true}
+        // onChange={e => handleSingleCheck(e.target.checked, id)}
+        // checked={checkItems.includes(id)}
+      />
+      <img className="cartLectureImg" src={image} alt="lectureImg" />
+      <div className="cartProductInfo">
+        <div className="productName">{title}</div>
+        <div className="productCount">
+          <span className="productCountHeader">수량</span>
+          <div className="productCountBtn">
+            <button
+              className="btn"
+              onClick={() => decreaseBtn(productId, quantity)}
+            >
+              －
+            </button>
+            <span className="quantity">{quantity}</span>
+            <button
+              className="btn"
+              onClick={() => increaseBtn(productId, quantity)}
+            >
+              ＋
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="cartProductPrice">
+        <span className="productPrice">
+          {(price * quantity).toLocaleString()}
+        </span>
+        원
+        <button className="cartDeleteBtn" onClick={() => deleteBtn(id)}>
+          ✖
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartProductList;
